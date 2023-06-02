da83c29a51a8064023a1b10c5615d464ff08f00b
xkochevnikx <xkochevnik@protonmail.com>
2023-06-02 14:33:23 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,36 +1,39 @@
-let inpWeather = document.querySelector("#city_name");
-let btnWeather = document.querySelector("#show");
-let listWeather = document.querySelector("#weather_list");
-let weatherWatchBox = document.querySelector(".weather_watch");
-let weatherDays = document.querySelector("#weather_threeDays");
-let weatherClose = document.querySelector("#closeWeather");
+let inpWeather = document.querySelector('#city_name');
+let btnWeather = document.querySelector('#show');
+let listWeather = document.querySelector('#weather_list');
+let weatherWatchBox = document.querySelector('#weather_watch');
+let weatherDays = document.querySelector('#weather_threeDays');
+let weatherClose = document.querySelector('#closeWeather');
 
-forecastWeather();
-function forecastWeather() {
-  btnWeather.addEventListener("click", () => {
-    const API = "https://goweather.herokuapp.com/weather/";
+btnWeather.addEventListener('click', () => {
+    const API = 'https://goweather.herokuapp.com/weather/';
     const apiSearch = fetch(`${API}${inpWeather.value}`);
     apiSearch
-      .then(response => response.json())
-      .then(elem => {
-        if (elem.temperature === "") {
-          alert("Ваш город не найден, попробуйте еще раз.");
-          return;
-        }
-        let temperature = document.createElement("li");
-        temperature.innerText = `Температура воздуха сейчас ${elem.temperature}`;
-        listWeather.append(temperature);
-        let wind = document.createElement("li");
-        wind.innerText = `Скорость ветра ${elem.wind}`;
-        listWeather.append(wind);
-        let WTD = document.createElement("li");
-        WTD.innerText = `Завтра ожидается температура ${elem.forecast[0].temperature} со скоростью ветра ${elem.forecast[0].wind}`;
-        weatherDays.append(WTD);
-        weatherClose.addEventListener("click", () => {
-          inpWeather.value = "";
-          listWeather.innerHTML = "";
-          weatherDays.innerHTML = "";
+        .then((response) => response.json())
+        .then((elem) => {
+            console.log(elem);
+            if (elem.temperature === '') {
+                alert('Ваш город не найден, попробуйте еще раз.');
+                return;
+            }
+            listWeather.innerHTML = '';
+            weatherDays.innerHTML = '';
+            let temperature = document.createElement('li');
+            let wind = document.createElement('li');
+            let WTD = document.createElement('li');
+
+            temperature.innerText = `Air temperature now ${elem.temperature}`;
+            wind.innerText = `Wind speed ${elem.wind}`;
+            WTD.innerText = `Temperature expected tomorrow ${elem.forecast[0].temperature} with the speed of the wind ${elem.forecast[0].wind}`;
+
+            listWeather.append(temperature);
+            listWeather.append(wind);
+            weatherDays.append(WTD);
+
+            weatherClose.addEventListener('click', () => {
+                inpWeather.value = '';
+                listWeather.innerHTML = '';
+                weatherDays.innerHTML = '';
+            });
         });
-      });
-  });
-}
+});
